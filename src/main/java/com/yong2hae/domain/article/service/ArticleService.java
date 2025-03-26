02fe9a4fc62982d2a098839e83d5767c7f5b3095
JaeHee-devSpace <jaehee1658@gmail.com>
2025-03-26 19:35:07 +0900

    feat: article 전체조회 서비스 로직 추가

--- src/main/java/com/yong2hae/domain/article/service/ArticleService.java
+++ src/main/java/com/yong2hae/domain/article/service/ArticleService.java
@@ -1,7 +1,44 @@
 package com.yong2hae.domain.article.service;
 
+import com.yong2hae.domain.article.dto.ArticleDetailResponseDTO;
+import com.yong2hae.domain.article.dto.ArticleResponseDTO;
+import com.yong2hae.domain.article.entitiy.Article;
+import com.yong2hae.domain.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    //article 전체 조회
+    public List<ArticleResponseDTO> getAllArticles(){
+        List<Article> articles = articleRepository.findAll();
+
+        List<ArticleResponseDTO> articlesDTO = articles.stream()
+                .map(ArticleResponseDTO::of)
+                .toList();
+
+        return articlesDTO;
+    }
+
+    //article 상세 조회
+    public ArticleDetailResponseDTO getArticleById(Long articleId) {
+        // articleId로 Article을 찾기
+        Article article = articleRepository.findById(articleId)
+                .orElseThrow(() -> new RuntimeException("Article not found with id: " + articleId));
+        // 찾은 Article을 DTO로 변환해서 반환
+        return ArticleDetailResponseDTO.of(article);
+    }
+
+    //article 삭제
+    //리턴값 없어도 되는지? 삭제 확인 해야할거같은데...?
+    public void deleteArticleById(Long articleId) {
+        // articleId로 Article을 삭제
+        articleRepository.deleteById(articleId);
+    }
 }
